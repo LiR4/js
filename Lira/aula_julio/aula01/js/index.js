70cd35477be3e4032e737543e36d53ae918812f3
Pedro Henrique Lira Da Silva Bispo <83731045+LiR4@users.noreply.github.com>
2022-01-21 17:17:17 -0300

    sei la

---
+++ Lira/aula_julio/aula01/js/index.js
@@ -0,0 +1,61 @@
+
+// var a = prompt("informe o primero numero")
+// var b = prompt("informe o sinal que vai ser utilizado (+,-,*,/,**)")
+// var c = prompt("informe o segundo numero")
+
+// var a1 = parseFloat(a)
+// var c1 = parseFloat(c)
+
+// if (b == "+") {
+//     console.log(a1 + c1) 
+// }else if (b == "-") {
+//     console.log(a1 - c1)
+// }else if (b == "/"){
+//     console.log(a1 / c1)
+// }else if (b == "*"){
+//     console.log(a1*c1)
+// }else if (b == "**") {
+//     console.log(a1 ** c1) 
+// }else{
+//     alert("erro")
+// }
+
+// switch (b) {
+//     case "+":
+//         console.log(a1 + c1)
+//         break;
+//     case "-":
+//         console.log(a1 - c1)
+//         break;
+//     case "*":
+//         console.log(a1 * c1)
+//         break;
+//     case "/":
+//         console.log(a1 / c1)
+//         break;
+//     case "**":
+//         console.log(a1 ** c1)
+//         break;
+//     default:
+//         alert("erro")
+//         break;
+// }
+
+// function vasco() {
+//     for (let a = 3; a >= 0 ; a--) {
+//         for (let b = 0; b < 60; b++) {
+//             console.log(a + ":" + b)
+//         }
+        
+//     } 
+// }
+
+function temp() {
+    var celsius = prompt("informe a temperatura em celsius")
+
+    var temp = parseFloat(celsius)
+
+    f = (temp * 1.8) + 32
+    alert(f)
+}
+
